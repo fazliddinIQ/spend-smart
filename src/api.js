3a366d3c4ca5ce6c582913671e8a9a98304e2f8a
Xafizaxon fazzliddin <xapizaxonraxmonova3@gmail.com>
2026-05-19 12:29:33 +0500

    add url

--- src/api.js
+++ src/api.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  // baseURL: "http://localhost:5000/api",
+  baseURL: "https://smart-spend-backend-2.onrender.com/api",
 });
 
 // token qo‘shib yuborish
